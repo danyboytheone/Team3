1b1d04f39a198b84cbf3959af056b707018eb12f
Daniel E <90887838+danyboytheone@users.noreply.github.com>
2023-10-09 02:56:00 -0700

    Add files via upload

---
+++ Button.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MyComponent() {
+  return (
+    <div>
+      <p>Button component.</p>
+      <button>Click me</button>
+    </div>
+  );
+}
+
+export default MyComponent;
